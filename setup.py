60d491aac57564fe8178e3c15844e63c89223b1b
Carson Owlett <35844973+5C4R48Security@users.noreply.github.com>
2019-04-30 13:30:31 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='shared',
+      version='0.0.2',
+      description='This package allows easy parsing and serialization of database dumps and leaks.',
+      author='Carson Owlett',
+      author_email='Not given',
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      license='LICENSE.txt',
+    )
